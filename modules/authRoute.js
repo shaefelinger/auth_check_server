28060f84f70bc0096a5d1bee454fc0bf92510b5f
Steffen Häfelinger <s.haefelinger@gmx.de>
2021-06-27 19:39:08 +0200

    cleaned up db

--- modules/authRoute.js
+++ modules/authRoute.js
@@ -10,36 +10,6 @@ router.get('/', (req, res) => {
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-// // Mongoose
-// // ==========================================================================
-// const mongoose = require('mongoose');
-
-// const url =
-//   'mongodb+srv://dbUser:' +
-//   process.env.MONGO_PW +
-//   '@cluster0.dq3y7.mongodb.net/' +
-//   process.env.MONGO_DB;
-
-// mongoose
-//   .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(console.log('Database ready'))
-//   .catch((err) => {
-//     console.error('😱Connection to MongoDB went wrong : ' + err);
-//   });
-// // ==========================================================================
-
-// // Users
-// // ==========================================================================
-
-// const userSchema = mongoose.Schema({
-//   name: String,
-//   email: String,
-//   password: String,
-//   profilePic: String,
-// });
-
-// const User = mongoose.model('User', userSchema);
-
 ///////////////////// List of ALL users
 router.get('/list', (req, res) => {
   User.find((err, foundUsers) => {
